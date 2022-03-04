0e103ff2038d80b8d457e0d07120fa3a2be39f7d
arthurchieppe <arthurgc1@al.insper.edu.br>
2022-03-04 16:40:02 -0300

    Conceito C

--- Lab2-PIO-Driver/src/main.c
+++ Lab2-PIO-Driver/src/main.c
@@ -73,6 +73,38 @@
 #define LED3_OLED_PIO_IDX	2
 #define LED3_OLED_PIO_IDX_MASK  (1 << LED3_OLED_PIO_IDX)
 
+
+
+/************************************************************************/
+/* constants                                                            */
+/************************************************************************/
+/*  Default pin configuration (no attribute). */
+#define _PIO_DEFAULT             (0u << 0)
+/*  The internal pin pull-up is active. */
+#define _PIO_PULLUP              (1u << 0)
+/*  The internal glitch filter is active. */
+#define _PIO_DEGLITCH            (1u << 1)
+/*  The internal debouncing filter is active. */
+#define _PIO_DEBOUNCE            (1u << 3)
+
+/************************************************************************/
+/* variaveis globais                                                    */
+/************************************************************************/
+
+/************************************************************************/
+/* prototypes                                                           */
+/************************************************************************/
+
+void init(void);
+
+/************************************************************************/
+/* interrupcoes                                                         */
+/************************************************************************/
+
+/************************************************************************/
+/* funcoes                                                              */
+/************************************************************************/
+
 //LAB 2:
 /**
  * \brief Set a high output level on all the PIOs defined in ul_mask.
@@ -110,31 +142,77 @@ void _pio_clear(Pio *p_pio, const uint32_t ul_mask)
  */
 void _pio_pull_up(Pio *p_pio, const uint32_t ul_mask,
         const uint32_t ul_pull_up_enable){
-	p_pio->PIO_PUER = ul_mask;
+	if (ul_pull_up_enable == 1) {
+		p_pio->PIO_PUER = ul_mask;
+	} else {
+		p_pio->PIO_PUDR = ul_mask;
+	}
+	
  }
+ 
+/**
+ * \brief Configure one or more pin(s) or a PIO controller as inputs.
+ * Optionally, the corresponding internal pull-up(s) and glitch filter(s) can
+ * be enabled.
+ *
+ * \param p_pio Pointer to a PIO instance.
+ * \param ul_mask Bitmask indicating which pin(s) to configure as input(s).
+ * \param ul_attribute PIO attribute(s).
+ */
+void _pio_set_input(Pio *p_pio, const uint32_t ul_mask, const uint32_t ul_attribute)
+{
+	_pio_pull_up(p_pio, ul_mask, ul_attribute & PIO_PULLUP);
+	if (ul_attribute & PIO_DEBOUNCE) {
+		p_pio->PIO_IFSCDR = ul_mask;
+		} else {
+		if (ul_attribute & PIO_DEGLITCH) {
+			p_pio->PIO_IFSCER = ul_mask;
+		}
+	}
+	if (ul_attribute && (PIO_DEGLITCH || PIO_DEBOUNCE)) {
+		p_pio->PIO_IFER = ul_mask;
+		} else {
+		p_pio->PIO_IFDR = ul_mask;
+	}
+}
 
-/************************************************************************/
-/* constants                                                            */
-/************************************************************************/
+/**
+ * \brief Configure one or more pin(s) of a PIO controller as outputs, with
+ * the given default value. Optionally, the multi-drive feature can be enabled
+ * on the pin(s).
+ *
+ * \param p_pio Pointer to a PIO instance.
+ * \param ul_mask Bitmask indicating which pin(s) to configure.
+ * \param ul_default_level Default level on the pin(s).
+ * \param ul_multidrive_enable Indicates if the pin(s) shall be configured as
+ * open-drain.
+ * \param ul_pull_up_enable Indicates if the pin shall have its pull-up
+ * activated.
+ */
+void _pio_set_output(Pio *p_pio, const uint32_t ul_mask,
+        const uint32_t ul_default_level,
+        const uint32_t ul_multidrive_enable,
+        const uint32_t ul_pull_up_enable)
+{
+	pio_pull_up(p_pio, ul_mask, ul_pull_up_enable);
+	if (ul_default_level) {
+		_pio_set(p_pio, ul_mask);
+	} else {
+		_pio_clear(p_pio, ul_mask);
+	}
+	
+	if (ul_multidrive_enable) {
+		p_pio->PIO_MDER = ul_mask;
+	} else {
+		p_pio->PIO_MDDR = ul_mask;
+	}
+	p_pio->PIO_OER = ul_mask;
+	p_pio->PIO_PER = ul_mask;
+}
 
-/************************************************************************/
-/* variaveis globais                                                    */
-/************************************************************************/
 
-/************************************************************************/
-/* prototypes                                                           */
-/************************************************************************/
 
-void init(void);
-
-/************************************************************************/
-/* interrupcoes                                                         */
-/************************************************************************/
-
-/************************************************************************/
-/* funcoes                                                              */
-/************************************************************************/
-
+ 
 // Função de inicialização do uC
 void init(void){
 	// Initialize the board clock
@@ -160,14 +238,20 @@ void init(void){
 	pio_set_output(LED3_OLED_PIO, LED3_OLED_PIO_IDX_MASK, 0, 0, 0);
 	// configura pino ligado ao botão como entrada com um pull-up.
 	//pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_DEFAULT);
-	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
-	pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
-	pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
-	// Inicializa botao como energizado:
-	//_pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
-	_pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
-	_pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
-	_pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
+// 	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_DEFAULT);
+// 	pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_DEFAULT);
+// 	pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_DEFAULT);
+// 	// Inicializa botao como energizado:
+// 	//_pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
+// 	_pio_pull_up(BUT1_PIO, BUT1_PIO_IDX_MASK, 1);
+// 	_pio_pull_up(BUT2_PIO, BUT2_PIO_IDX_MASK, 1);
+// 	_pio_pull_up(BUT3_PIO, BUT3_PIO_IDX_MASK, 1);
+	_pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, _PIO_PULLUP | _PIO_DEBOUNCE);
+	_pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, _PIO_PULLUP | _PIO_DEBOUNCE);
+	_pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, _PIO_PULLUP | _PIO_DEBOUNCE);
+
+
+
 
 
 }
@@ -218,41 +302,7 @@ int main(void)
 			_pio_set(LED2_OLED_PIO, LED2_OLED_PIO_IDX_MASK);
 			_pio_set(LED3_OLED_PIO, LED3_OLED_PIO_IDX_MASK);
 		}
-	/*} else if (!pio_get(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK)) {
-		for (int i =0; i < 10; i++) {
-			_pio_set(LED2_OLED_PIO, LED2_OLED_PIO_IDX_MASK);      // Coloca 1 no pino LED
-			delay_ms(200);                        // Delay por software de 200 ms
-			_pio_clear(LED2_OLED_PIO, LED2_OLED_PIO_IDX_MASK);    // Coloca 0 no pino do LED
-			delay_ms(200);                        // Delay por software de 200 ms
-		}
-		
-	  
-	_pio_set(LED1_OLED_PIO, LED1_OLED_PIO_IDX_MASK);
-	_pio_set(LED2_OLED_PIO, LED2_OLED_PIO_IDX_MASK); 
-	_pio_set(LED3_OLED_PIO, LED3_OLED_PIO_IDX_MASK);       // Coloca 1 no pino LED
-	delay_ms(200);                        // Delay por software de 200 ms
-	_pio_clear(LED1_OLED_PIO, LED1_OLED_PIO_IDX_MASK);
-	_pio_clear(LED2_OLED_PIO, LED2_OLED_PIO_IDX_MASK); 
-	_pio_clear(LED3_OLED_PIO, LED3_OLED_PIO_IDX_MASK);     // Coloca 0 no pino do LED
-	delay_ms(200);                        // Delay por software de 200 ms
-		
-		
-	*/	
   }
   return 0;
 }
 
-//Codigo anterior entrega obrigatoria:
-/*
-if (!pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK)) {
-	for (int i =0; i < 10; i++) {
-		
-		_pio_set(LED_PIO, LED_PIO_IDX_MASK);      // Coloca 1 no pino LED
-		delay_ms(200);                        // Delay por software de 200 ms
-		_pio_clear(LED_PIO, LED_PIO_IDX_MASK);    // Coloca 0 no pino do LED
-		delay_ms(200);                        // Delay por software de 200 ms
-	}
-	} else {
-	_pio_set(LED_PIO, LED_PIO_IDX_MASK);
-}
-*/
