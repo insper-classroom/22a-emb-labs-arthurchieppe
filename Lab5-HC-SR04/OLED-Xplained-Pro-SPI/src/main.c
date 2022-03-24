8c3dbe5b90437e5d786d95e993e913483bafb0c9
arthurchieppe <arthurgc1@al.insper.edu.br>
2022-03-23 22:56:55 -0300

    Recomecando, botao funcionando

--- Lab5-HC-SR04/OLED-Xplained-Pro-SPI/src/main.c
+++ Lab5-HC-SR04/OLED-Xplained-Pro-SPI/src/main.c
@@ -4,12 +4,6 @@
 #include "gfx_mono_text.h"
 #include "sysfont.h"
 
-// Configuracoes do botao da placa OLED:
-//#define BUT1_PIO				PIOD
-//#define BUT1_PIO_ID				ID_PIOD
-//#define BUT1_PIO_IDX			28
-//#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX) // esse já está pronto.
-
 //Pino X
 #define X_PIO					PIOD
 #define X_PIO_ID				ID_PIOD
@@ -21,6 +15,12 @@
 #define Y_PIO_ID				ID_PIOA
 #define Y_PIO_IDX				3
 #define Y_PIO_IDX_MASK			(1u << Y_PIO_IDX)
+
+// Configuracoes do botao da placa OLED:
+#define BUT1_PIO				PIOD
+#define BUT1_PIO_ID				ID_PIOD
+#define BUT1_PIO_IDX			28
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX) // esse já está pronto.
 
 // LED1 placa oled
 #define LED1_OLED_PIO		PIOA
@@ -40,18 +40,15 @@
 #define LED3_OLED_PIO_IDX	2
 #define LED3_OLED_PIO_IDX_MASK  (1 << LED3_OLED_PIO_IDX)
 
-	
-//TC
-void LED_init(int estado);
-void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
-void pin_toggle(Pio *pio, uint32_t mask);
-volatile char counter = 0;
+//Global chars:
+volatile char echo_fall_flag = 1;
+volatile char echo_rise_flag = 0;
+volatile char but1_flag = 0;
 
 //RTT
 void io_init(void);
 static void RTT_init(float freqPrescale, uint32_t IrqNPulses, uint32_t rttIRQSource);
 
-void pisca_led(int n, int t);
 
 void pin_toggle(Pio *pio, uint32_t mask) {
 	if(pio_get_output_data_status(pio, mask))
@@ -78,46 +75,6 @@ void LED_init(int estado) {
 	pio_set_output(LED3_OLED_PIO, LED3_OLED_PIO_IDX_MASK, estado, 0, 0);
 }
 
-void TC1_Handler(void) {
-	/**
-	* Devemos indicar ao TC que a interrupção foi satisfeita.
-	* Isso é realizado pela leitura do status do periférico
-	**/
-	volatile uint32_t status = tc_get_status(TC0, 1);
-
-	/** Muda o estado do LED (pisca) **/
-	pin_toggle(LED1_OLED_PIO, LED1_OLED_PIO_IDX_MASK);  
-}
-
-void TC3_Handler(void) {
-	/**
-	* Devemos indicar ao TC que a interrupção foi satisfeita.
-	* Isso é realizado pela leitura do status do periférico
-	**/
-	volatile uint32_t status = tc_get_status(TC1, 0);
-	if (but1_flag) {
-		counter += 1;
-	}
-}
-
-void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq){
-	uint32_t ul_div;
-	uint32_t ul_tcclks;
-	uint32_t ul_sysclk = sysclk_get_cpu_hz();
-
-	/* Configura o PMC */
-	pmc_enable_periph_clk(ID_TC);
-
-	/** Configura o TC para operar em  freq hz e interrupçcão no RC compare */
-	tc_find_mck_divisor(freq, ul_sysclk, &ul_div, &ul_tcclks, ul_sysclk);
-	tc_init(TC, TC_CHANNEL, ul_tcclks | TC_CMR_CPCTRG);
-	tc_write_rc(TC, TC_CHANNEL, (ul_sysclk / ul_div) / freq);
-
-	/* Configura NVIC*/
-	NVIC_SetPriority(ID_TC, 4);
-	NVIC_EnableIRQ((IRQn_Type) ID_TC);
-	tc_enable_interrupt(TC, TC_CHANNEL, TC_IER_CPCS);
-}
 
 void RTT_Handler(void) {
 	uint32_t ul_status;
@@ -176,33 +133,23 @@ void but1_callback(void) {
 	but1_flag = 1;
 }
 
-void configure_button(Pio *p_pio, const uint32_t ul_mask, uint32_t ul_id, void (*p_handler) (uint32_t, uint32_t)) {
-	pmc_enable_periph_clk(ul_id);
-
-	// Configura PIO para lidar com o pino do bot?o como entrada
-	// com pull-up
-	pio_configure(p_pio, PIO_INPUT, ul_mask, PIO_PULLUP | PIO_DEBOUNCE);
-	pio_set_debounce_filter(p_pio, ul_mask, 60);
-
-	// Configura interrup??o no pino referente ao botao e associa
-	// fun??o de callback caso uma interrup??o for gerada
-	// a fun??o de callback ? a: but_callback()
-	pio_handler_set(p_pio,
-	ul_id,
-	ul_mask,
-	PIO_IT_RISE_EDGE,
-	p_handler
-	);
-
-	// Ativa interrup??o e limpa primeira IRQ gerada na ativacao
-	pio_enable_interrupt(p_pio, ul_mask);
-	pio_get_interrupt_status(p_pio);
+void io_init(void) {
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+	pio_set_debounce_filter(BUT1_PIO, BUT1_PIO_IDX_MASK, 60);
 	
-	// Configura NVIC para receber interrupcoes do PIO do botao
-	// com prioridade 4 (quanto mais pr?ximo de 0 maior)
-	NVIC_EnableIRQ(ul_id);
-	NVIC_SetPriority(ul_id, 4); // Prioridade 4
-}
+	pio_handler_set(BUT1_PIO,
+	BUT1_PIO_ID,
+	BUT1_PIO_IDX_MASK,
+	PIO_IT_RISE_EDGE,
+	but1_callback);
+	
+	pio_enable_interrupt(BUT1_PIO, BUT1_PIO_IDX_MASK);
+	pio_get_interrupt_status(BUT1_PIO);
+	
+	NVIC_EnableIRQ(BUT1_PIO_ID);
+	NVIC_SetPriority(BUT1_PIO_ID, 3);
+}	
 
 
 int main (void)
@@ -218,24 +165,22 @@ int main (void)
   // Init OLED
 	gfx_mono_ssd1306_init();
 	
-	//LED_init(1);
-	//TC
-	//TC_init(TC0, ID_TC1, 1, 4);
-	//TC_init(TC1, ID_TC3, 0, 1);
-	//tc_start(TC0, 1);
-	//tc_start(TC1, 0);
+	LED_init(1);
+	
 	//RTT
 	RTT_init(1, 2, RTT_MR_ALMIEN); 
 	
-	/* Leitura do valor atual do RTC */
 	char str[128];
   
 
   /* Insert application code here, after the board has been initialized. */
 	while(1) {
-		//gfx_mono_draw_string("           ", 0, 0, &sysfont);
-		//sprintf(str, "%d:%d:0%d", current_hour, current_min, current_sec);
-		//gfx_mono_draw_string(str, 0,0, &sysfont);
+		if (but1_flag) {
+			gfx_mono_draw_string("           ", 0, 0, &sysfont);
+			sprintf(str, "ED %d", 1);
+			gfx_mono_draw_string(str, 0,0, &sysfont);
+			
+		}
 		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
 	}
 }
